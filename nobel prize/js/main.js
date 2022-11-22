15299bb896038dc4ffa615af2fe169981823d392
uz3ix <113834959+uz3ix@users.noreply.github.com>
2022-11-22 22:51:44 +0300

    Add files via upload

---
+++ nobel prize/js/main.js
@@ -0,0 +1,32 @@
+const sliderBox = document.querySelector('.slider-box');
+const slideLeft = document.querySelector('.left-slide');
+const slideRight = document.querySelector('.right-slide');
+const upBtn = document.querySelector('.up-btn');
+const downBtn = document.querySelector('.down-btn');
+const slidesLength = slideRight.querySelectorAll('div').length;
+
+let activeSlideIndex = 0;
+
+slideLeft.style.top = `-${(slidesLength - 1) * 100}vh`;
+
+upBtn.addEventListener('click', () => changeSlide('up'));
+downBtn.addEventListener('click', () => changeSlide('down'));
+
+const changeSlide = (direction) => {
+    const sliderHeight = sliderBox.clientHeight;
+    if(direction === 'up'){
+        activeSlideIndex++;
+        if(activeSlideIndex > slidesLength - 1){
+            activeSlideIndex = 0;
+        }
+    }
+    else if(direction === 'down'){
+        activeSlideIndex--;
+        if(activeSlideIndex < 0){
+            activeSlideIndex = slidesLength - 1;
+        }
+    }
+
+    slideRight.style.transform = `translateY(-${activeSlideIndex * sliderHeight}px)`;
+    slideLeft.style.transform = `translateY(${activeSlideIndex * sliderHeight}px)`;
+}
